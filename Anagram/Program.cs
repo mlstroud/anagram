bd26d1cdae722852776c53f91129907b20875faf
Matt Stroud <lixingke06@gmail.com>
2020-05-12 11:33:04 -0700

    add user interface in console to gather input and display results

--- Anagram/Program.cs
+++ Anagram/Program.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using Anagrams.Models;
 
 namespace Anagrams
 {
@@ -5,7 +8,49 @@ namespace Anagrams
   {
     public static void Main()
     {
+      string userInput = "";
+      List<string> anagrams;
+      userInput = PromptUser();
+      Anagram.Word = userInput;
+      GatherWordList();
+      anagrams = Anagram.GetAnagrams();
+      DisplayAnagrams(anagrams);
+    }
 
+    public static string PromptUser()
+    {
+      Console.WriteLine("Enter a word to start");
+      Console.WriteLine("---------------------");
+      string input = Console.ReadLine();
+      return input.ToLower();
+    }
+
+    public static void GatherWordList()
+    {
+      string userInput = "";
+      Console.WriteLine("Enter words one at a time to compare against. Enter 'Q' when done.");
+
+      while (userInput != "q")
+      {
+        userInput = Console.ReadLine().ToLower();
+        Anagram.Add(userInput);
+      }
+    }
+
+    public static void DisplayAnagrams(List<string> anagrams)
+    {
+      if (anagrams.Count == 0)
+      {
+        Console.WriteLine("There were no anagrams.");
+      }
+      else
+      {
+        Console.WriteLine("Anagrams for " + Anagram.Word);
+        for (int i = 0; i < anagrams.Count; i++)
+        {
+          Console.WriteLine((i + 1) + ". " + anagrams[i]);
+        }
+      }
     }
   }
 }
