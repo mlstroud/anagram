6cecb0945a80bb5a12c695f23762463be4bec246
Matt Stroud <lixingke06@gmail.com>
2020-05-12 10:32:20 -0700

    add code to create a list of words and return it

--- Anagram/Models/Anagram.cs
+++ Anagram/Models/Anagram.cs
@@ -1,13 +1,20 @@
+using System.Collections.Generic;
 
 namespace Anagrams.Models
 {
   public class Anagram
   {
     public string Word { get; set; }
+    private static List<string> _wordList = new List<string>();
 
     public Anagram(string word)
     {
       Word = word;
+    }
+
+    public static List<string> GetList()
+    {
+      return _wordList;
     }
   }
 }
